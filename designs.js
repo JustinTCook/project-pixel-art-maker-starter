c7f5946c5c58c4212210e9a179da58f23b9b5561
Justin Cook <jtcook19@gmail.com>
2018-01-06 15:46:24 -0700

    Completed exercise

--- designs.js
+++ designs.js
@@ -1,10 +1,38 @@
 // Select color input
+const colorPicker = document.getElementById('colorPicker');
+
 // Select size input
+const inputHeight = document.getElementById('input_height');
+const inputWidth = document.getElementById('input_width');
 
 // When size is submitted by the user, call makeGrid()
+document.getElementById('sizePicker').addEventListener("submit", function(evt){
+  evt.preventDefault();
+  makeGrid();
+});
 
 function makeGrid() {
 
-// Your code goes here!
+  var table = document.getElementById('pixel_canvas');
+
+  // delete old rows
+  while(table.rows.length > 0)
+    table.deleteRow(0);
+
+  // Create rows
+  for (var i = 0; i <= inputHeight.value - 1; i++){
+    var row = table.insertRow(i);
+
+    // Create cells
+    for (var j = 0; j <= inputWidth.value - 1; j++){
+      var cell = row.insertCell(j);
+
+      // Add listener to cell
+      cell.addEventListener('click', function(e){
+        e.target.style.backgroundColor = colorPicker.value;
+        e.target.style.borderColor = colorPicker.value;
+      })
+    }
+  }
 
 }
